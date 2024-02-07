cfc61713a4b467ab5e211a96a1ea178d68939722
Joshua Aideloje <aidelojejoshua@gmail.com>
2024-02-07 14:56:14 +0100

    📦 NEW: Add @vercel/speed-insights package

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
